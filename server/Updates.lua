fb25cb8411d1d305e16bd95c76e43593c01df775
Nox <167952403+Tnoxious@users.noreply.github.com>
2024-04-29 01:11:17 +1000

    Update Updates.lua
    url to repo version checks

--- server/Updates.lua
+++ server/Updates.lua
@@ -5,7 +5,7 @@ if Config.CheckForUpdates then
     end
 
     local function CheckMenuVersion()
-        PerformHttpRequest('https://raw.githubusercontent.com/TayMcKenzieNZ/rpemotes/master/version.txt', function(err, text, headers)
+        PerformHttpRequest('https://github.com/Tnoxious/nox-rpemotes/blob/master/version.txt', function(err, text, headers)
             local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
             if not text then
                 VersionLog('error', 'Currently unable to run a version check.')
