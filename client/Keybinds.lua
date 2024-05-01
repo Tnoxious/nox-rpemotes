de79f2d1dde8c42559711e17db9f1d07c4a98614
Nox <167952403+Tnoxious@users.noreply.github.com>
2024-05-02 01:56:44 +1000

    last edit to headers

--- client/Keybinds.lua
+++ client/Keybinds.lua
@@ -1,4 +1,6 @@
---- RPEmotes maintained by TayMcKenzieNZ, Mathu_lmn, MadsL, MLGCrisis, Jimathy, Tnoxious, alberttheprince and roleplay Community ---
+              -- THIS PROJECT IS BEEN MOVED TO A NEW MENU/NAME THAT WILL BE OUT SOON THIS WILL BE LAST UPDATES v1.2.0B ---
+                      -- New menu will be added to https://github.com/Tnoxious?tab=repositories --
+       --- RPEmotes maintained by TayMcKenzieNZ, Mathu_lmn, MadsL, MLGCrisis, Jimathy, Tnoxious and roleplay Community ---      
 --- Leakers and resellers are the absolute scum of the earth RPEmotes will always be free!! We above support open source code ---
 
 if Config.SqlKeybinding then
